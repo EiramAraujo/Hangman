9afcd00eaf91bc88ba0ad4bceb619d43d8fd796d
Eiram Araujo <eiramaraujo1998@gmail.com>
2019-08-20 10:07:00 -0500

    Fixed variables, started startGame function.
    Signed-off-by: Eiram Araujo <eiramaraujo1998@gmail.com>

--- src/Hangman/Game.scala
+++ src/Hangman/Game.scala
@@ -1,16 +1,18 @@
 package Hangman
 
+import scala.collection.mutable.ArrayBuffer
+
 class Game {
   private var frase:String = new String()
   private var banco_letras:Array[Char] = new Array[Char](26)
   private var errores:Array[Char] = new Array[Char](7)
-  private var aciertos:Array[Char] = new Array[Char]()
-  private var letra:Char = _
-  private var lenght:Byte = _
-  private var num_errores:Byte = _
-  private var suposicion:Boolean = _
+  private var aciertos:ArrayBuffer[Char] = new ArrayBuffer[Char]()
+  private var letra:Char = null
+  private var lenght:Byte = null
+  private var num_errores:Byte = null
+  private var suposicion:Boolean = null
 
-  public void startGame(){
+  def startGame(){
 
   }
 
